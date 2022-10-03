f67615cd63cebac90e21a53a5ba9799043922b31
sakshi26072001 <74151176+sakshi26072001@users.noreply.github.com>
2022-10-03 16:08:06 +0530

    pass 1

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(64), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.now)
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    admin
