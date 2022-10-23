6de158c608af34549942006e3720d7213eea5a8f
sakshi26072001 <74151176+sakshi26072001@users.noreply.github.com>
2022-10-23 12:42:03 +0530

    nm

--- app.py
+++ app.py
@@ -20,11 +20,22 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.now)
+
 class Admin(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     adminname = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(64), nullable=False)
+    created_on = db.Column(db.DateTime, default=datetime.now)   
+
+class Quiz(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question= db.Column(db.String(1024), unique=True, nullable=False)
+    option_A = db.Column(db.String(500), unique=True, nullable=False)
+    option_B = db.Column(db.String(500), unique=True, nullable=False)
+    option_C = db.Column(db.String(500), unique=True, nullable=False)
+    option_D = db.Column(db.String(500), unique=True, nullable=False)
+    answer=db.Column(db.String(500),nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.now)    
 
     def __str__(self):
@@ -38,14 +49,6 @@ def create_app():
     app.secret_key = 'supersecretkeythatnooneknows'
     db.init_app(app)
     return app
-def create_adm():
-    adm = Flask(__name__)
-    adm.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/app.sqlite'
-    adm.config['SQLALCHEMY_ECHO'] = True
-    adm.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    adm.secret_key = 'supersecretkeythatnooneknows'
-    db.init_app(adm)
-    return app      
 
 app = create_app()
 
@@ -54,6 +57,13 @@ def create_login_session(user: User):
     session['username'] = user.username
     session['email'] = user.email
     session['is_logged_in'] = True
+
+def create_admin_session(admin:Admin):
+    session['id'] = admin.id
+    session['username'] = admin.adminname
+    session['email'] = admin.email
+    session['is_logged_in'] = True
+    session['is_admin'] = True   
    
 
 def destroy_login_session():
@@ -122,34 +132,38 @@ def register():
             flash('user account could not be created','warning')
     return render_template('register.html', error_list=errors)
 
-@app.route('/admin_register', methods=['GET','POST'])
+@app.route('/admin_login', methods=['GET','POST'])
 def admin_register():
-    errors = []
-    if request.method == 'POST': # if form was submitted
-        adminname = request.form.get('adminname')
-        email = request.form.get('email')
-        pwd = request.form.get('password')
-        cpwd = request.form.get('confirmpass')
-        print(adminname, email, pwd, cpwd)
-        if adminname and email and pwd and cpwd:
-            if len(adminname)<2:
-                errors.append("Adminname is too small")
-            if len(email) < 11 or '@' not in email:
-                errors.append("Email is invalid")
-            if len(pwd) < 6:
-                errors.append("Password should be 6 or more chars")
-            if pwd != cpwd:
-                errors.append("passwords do not match")
-            if len(errors) == 0:
-                admin = Admin(adminname=adminname, email=email, password=pwd)
-                db.session.add(admin)
-                db.session.commit()
-                flash('Admin account created','success')
-                return redirect('/login')
-        else:
-            errors.append('Fill all the fields')
-            flash('admin account could not be created','warning')
-    return render_template('admin_register.html', error_list=errors)
+    if session.get('is_logged_in', False) and session.get('is_admin',False):
+        errors = []
+        if request.method == 'POST': # if form was submitted
+            adminname = request.form.get('adminname')
+            email = request.form.get('email')
+            pwd = request.form.get('password')
+            cpwd = request.form.get('confirmpass')
+            print(adminname, email, pwd, cpwd)
+            if adminname and email and pwd and cpwd:
+                if len(adminname)<2:
+                    errors.append("Adminname is too small")
+                if len(email) < 11 or '@' not in email:
+                    errors.append("Email is invalid")
+                if len(pwd) < 6:
+                    errors.append("Password should be 6 or more chars")
+                if pwd != cpwd:
+                    errors.append("passwords do not match")
+                if len(errors) == 0:
+                    admin = Admin(adminname=adminname, email=email, password=pwd)
+                    db.session.add(admin)
+                    db.session.commit()
+                    flash('Admin account created','success')
+                    return redirect('/login')
+            else:
+                errors.append('Fill all the fields')
+                flash('admin account could not be created','warning')
+        return render_template('admin_login.html', error_list=errors)
+    else:
+        flash('Login in admin to access this content','danger')
+        return redirect('/')
 
 @app.route('/logout')
 def logout():
