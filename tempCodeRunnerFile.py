f1366d6546cc20a1763fdae4177bab9d0beda115
sakshi26072001 <74151176+sakshi26072001@users.noreply.github.com>
2022-11-02 00:28:05 +0530

    n

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
-    # else:
-    #     flash('Login in admin to access this content','danger')
-    #     return redirect('/')
+    #cv2.putText(frame, "A", (w - 70, 30), font, 1, red, 2, cv2.LINE_AA)
+    
