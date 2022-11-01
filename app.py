3c461b5f3b1a748f58d43a197765bdc986298bc5
sakshi26072001 <74151176+sakshi26072001@users.noreply.github.com>
2022-11-01 13:26:07 +0530

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from flask import Flask, render_template, request, redirect, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -31,15 +32,16 @@ class Admin(db.Model):
 class Quiz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question= db.Column(db.String(1024), unique=True, nullable=False)
-    option_A = db.Column(db.String(500), unique=True, nullable=False)
-    option_B = db.Column(db.String(500), unique=True, nullable=False)
-    option_C = db.Column(db.String(500), unique=True, nullable=False)
-    option_D = db.Column(db.String(500), unique=True, nullable=False)
+    option_A = db.Column(db.String(500), nullable=False)
+    option_B = db.Column(db.String(500), nullable=False)
+    option_C = db.Column(db.String(500), nullable=False)
+    option_D = db.Column(db.String(500), nullable=False)
     answer=db.Column(db.String(500),nullable=False)
+    category = db.Column(db.String(80), nullable=False)
     created_on = db.Column(db.DateTime, default=datetime.now)    
 
     def __str__(self):
-        return f'{self.username}({self.id})'
+        return f'{self.question}'
 
 def create_app():
     app = Flask(__name__)
@@ -133,8 +135,35 @@ def register():
     return render_template('register.html', error_list=errors)
 
 @app.route('/admin_login', methods=['GET','POST'])
+def admin_login():
+    errors = {}
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        print("LOGGIN IN",email, password)
+        if password and email:
+            if len(email) < 11 or '@' not in email:
+                errors['email'] = 'Email is Invalid'
+            if len(errors) == 0:
+                admin = Admin.query.filter_by(email=email).first()
+                if admin is not None:
+                    print("admin account found", admin)
+                    if admin.password == password:
+                        create_admin_session(admin)
+                        flash('Login Successfull', "success")
+                        return redirect('/admin/dashboard')
+                    else:
+                        errors['password'] = 'Password is invalid'
+                else:
+                    errors['email']= 'Account does not exists'
+        else:
+            errors['email'] = 'Please fill valid details'
+            errors['password'] = 'Please fill valid details'
+    return render_template('admin_login.html', errors = errors)
+
+@app.route('/admin/register', methods=['GET','POST'])
 def admin_register():
-    if session.get('is_logged_in', False) and session.get('is_admin',False):
+    # if session.get('is_logged_in', False) and session.get('is_admin',False):
         errors = []
         if request.method == 'POST': # if form was submitted
             adminname = request.form.get('adminname')
@@ -156,11 +185,19 @@ def admin_register():
                     db.session.add(admin)
                     db.session.commit()
                     flash('Admin account created','success')
-                    return redirect('/login')
+                    return redirect('/admin_login')
             else:
                 errors.append('Fill all the fields')
                 flash('admin account could not be created','warning')
-        return render_template('admin_login.html', error_list=errors)
+        return render_template('admin_register.html', error_list=errors)
+     # else:
+    #     flash('Login in admin to access this content','danger')
+    #     return redirect('/')
+
+@app.route('/admin/dashboard', methods=['GET','POST'])    
+def admin():
+    if session.get('is_logged_in', False) and session.get('is_admin',False):
+        return render_template('admin_dashboard.html')
     else:
         flash('Login in admin to access this content','danger')
         return redirect('/')
@@ -170,6 +207,67 @@ def logout():
     destroy_login_session()
     flash('You are logged out','success')
     return redirect('/')    
+
+@app.route('/quiz/add', methods=['GET','POST'])
+def add_questions():
+    if session.get('is_logged_in', False) and session.get('is_admin',False):
+        errors = []
+        if request.method == 'POST':
+            question = request.form.get('question')
+            option_A = request.form.get('option_A')
+            option_B = request.form.get('option_B')
+            option_C = request.form.get('option_C')
+            option_D = request.form.get('option_D')
+            answer = request.form.get('answer')
+            category = request.form.get('category')
+            print(question, option_A, option_B, option_C, option_D, answer, category)
+            if question and option_A and option_B and option_C and option_D and answer and category:
+                if len(question)<2:
+                    errors.append("Question is too small")
+                if len(option_A) < 1:
+                    errors.append("Option A is invalid")
+                if len(option_B) < 1:
+                    errors.append("Option B is invalid")
+                if len(option_C) < 1:
+                    errors.append("Option C is invalid")
+                if len(option_D) < 1:
+                    errors.append("Option D is invalid")
+                if len(answer) < 1:
+                    errors.append("Answer is invalid")
+                if len(errors) == 0:
+                    quiz = Quiz(question=question, option_A=option_A, option_B=option_B, option_C=option_C, option_D=option_D, answer=answer, category=category)
+                    db.session.add(quiz)
+                    db.session.commit()
+                    flash('Question added','success')
+                    return redirect('/quiz/add')
+            else:
+                errors.append('Fill all the fields')
+                flash('Question could not be added','warning')
+        return render_template('add_question.html', error_list=errors)
+    else:
+        flash('Login in admin to access this content','danger')
+        return redirect('/')
+
+@app.route('/quiz/view')
+def view_questions():
+    if session.get('is_logged_in', False) and session.get('is_admin',False):
+        questions = Quiz.query.all()
+        return render_template('view_questions.html', questions=questions)
+    else:
+        flash('Login in admin to access this content','danger')
+        return redirect('/')
+
+@app.route('/quiz/delete/<int:id>')
+def delete_question(id):
+    if session.get('is_logged_in', False) and session.get('is_admin',False):
+        question = Quiz.query.get(id)
+        db.session.delete(question)
+        db.session.commit()
+        flash('Question deleted','success')
+        return redirect('/quiz/view')
+    else:
+        flash('Login in admin to access this content','danger')
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
--- camera_access.py
+++ camera_access.py
@@ -3,39 +3,79 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
+
+def distanceCalculate(p1, p2):
+    dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return dis
+font = cv2.FONT_HERSHEY_SIMPLEX
+red = (0, 0, 255)
+green = (0, 255, 0)
+blue= (255, 0, 0)
+white=(255, 255, 255)
 # For webcam input:
 cap = cv2.VideoCapture(0)
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
+    while cap.isOpened():
+        success, image = cap.read()
+       
+        if not success:
+            print("Ignoring empty camera frame.")
+            # If loading a video, use 'break' instead of 'continue'.
+            continue
+        h, w, _ = image.shape
+        image = cv2.flip(image, 1)
+        # To improve performance, optionally mark the image as not writeable to
+        # pass by reference.
+        image.flags.writeable = False
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = hands.process(image)
 
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    if results.multi_hand_landmarks:
-      for hand_landmarks in results.multi_hand_landmarks:
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
+        # Draw the hand annotations on the image.
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                coords_1 = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                coords_2 = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                try:
+                    # normalize the coordinates
+                    x1, y1 = int(coords_1.x * w), int(coords_1.y * h)
+                    x2, y2 = int(coords_2.x * w), int(coords_2.y * h)
+                    cv2.circle(image, (x1, y1), 5, (255, 0, 0), -1)
+                    cv2.circle(image, (x2, y2), 5, (0, 255, 0), -1)
+                    # calculate the distance
+                    dis = distanceCalculate((x1, y1), (x2, y2))
+                    # draw the distance at the bottom of the image
+                    cv2.putText(image, str(int(dis)), (20, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    # logic 
+                    if dis < 30:
+                        # display joined
+                        cv2.putText(image, "Selector", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    else:
+                        # display not joined
+                        cv2.putText(image, "Pointer", (20, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                except Exception as e:
+                    print(e)
+        # display quiz ui
+        q ="Is this a hand?"
+        cv2.putText(image, q, (30, h - 50), font, 1, white, 2, cv2.LINE_AA)
+        cv2.rectangle(image, (20, 90), (200,170), blue, 2)
+        cv2.putText(image, "category", (50, 130), font, 1, blue, 2, cv2.LINE_AA)
+        cv2.rectangle(image, (230, 20), (410,100), red, 2)
+        #cv2.putText(frame, "A", (w - 70, 30), font, 1, red, 2, cv2.LINE_AA)
+        cv2.rectangle(image, (440, 20), (620,100), red, 2)
+     # cv2.putText(frame, "B", (w - 70, 30), font, 1, red, 2, cv2.LINE_AA)
+        cv2.rectangle(image, (20, 200), (200,240), blue, 2)
+     # cv2.putText(frame, "score", (w - 70, 30), font, 1, red, 2, cv2.LINE_AA)
+        cv2.rectangle(image, (230,130), (410,210), red, 2)
+     # cv2.putText(frame, "B", (w - 70, 30), font, 1, red, 2, cv2.LINE_AA)
+        cv2.rectangle(image, (440, 130), (620,210), red, 2)
+     # cv2.putText(frame, "B", (w - 70, 30), font, 1, red, 2, cv2.LINE_AA)
+        cv2.imshow('MediaPipe Hands', image)
+        if cv2.waitKey(5) & 0xFF == 27:
+            break
+        
 cap.release()
-cv2.destroyAllWindows()
